c1249596bc6c5f87735cd3d40df8d44221502984
Valeria Rybakova <valerian.rybakova@yandex.ru>
2024-06-19 00:10:50 +0300

    refactor: скорректировать структуру проекта и нейминги

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import "./styles.css";
 import React, { useState } from "react";
-import Field from "./components/field";
-import Languages from "./components/languages";
-import Translate from "./components/translate";
-import Footer from "./components/footer";
+import Field from "./components/Field";
+import Languages from "./components/Languages";
+import Translate from "./components/Translate";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Header from "./components/Header";
 
 export default function App() {
   const [language, setLanguage] = useState("en");
   const [text, setText] = useState("Реакция");
 
   return (
-    <div>
-      <Field label="Введите русские слова" onChange={setText} value={text} />
-      <Languages language={language} onLanguageChange={setLanguage} />
-      <hr />
-      <Translate text={text} language={language} />
+    <div className="content">
+      <Header />
+      <Main>
+        <Field label="Введите слово на русском языке" onChange={setText} value={text} />
+        <Languages language={language} onLanguageChange={setLanguage} />
+        <hr />
+        <Translate text={text} language={language} />
+      </Main>
       <Footer />
     </div>
   );
