f6d365e7c8de8d6a0aefc820a8944077574db016
Valeria Rybakova <valerian.rybakova@yandex.ru>
2024-06-19 02:02:17 +0300

    Подготовить билд к деплою

---
+++ precache-manifest.e4a578e3bc44272d41477cfaf9db9722.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf5002e3092a8d205abbbf2eaee5754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcf391fafcc8422ba2aa",
+    "url": "/static/css/main.c9812214.chunk.css"
+  },
+  {
+    "revision": "0bfa8413fdaece8f154b",
+    "url": "/static/js/2.5f90178f.chunk.js"
+  },
+  {
+    "revision": "fcf391fafcc8422ba2aa",
+    "url": "/static/js/main.edc0e065.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
